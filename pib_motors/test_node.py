c58e0a3787d9de2f75dc230ee2233c6b01ea597d
mazeninvent <mazeninvent@gmail.com>
2023-05-07 22:49:02 +0300

    second commit

---
+++ pib_motors/test_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.brick_hat import BrickHAT
+from tinkerforge.brick_hat import BrickletServoV2
+
+class MyNode(Node): 
+class MyNode(Node): 
+
+def main(args=None):
+
+    rclpy.shutdpown()
+
+if __name__ == '__main__':
+    main()
